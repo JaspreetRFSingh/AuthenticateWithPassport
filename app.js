d8865c6dd8ddce7dea3ce7f571a89389224e67fb
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2020-01-31 13:51:09 +0530

    User info rendered from google api on view page

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const cookieSession = require('cookie-session');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const keys = require('./config/keys');
 const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+    maxAge: 24*60*60*1000,
+    keys: [keys.session.cookieKey]
+}));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
